119c7a7640006807c916858f8c7fe2f527c01558
Matt Phillips <matt@mattphillips.io>
2017-05-16 10:23:22 +0100

    Add read by id to dynamo interface

--- src/persistence/dynamoDb/index.js
+++ src/persistence/dynamoDb/index.js
@@ -9,6 +9,16 @@ const read = (client, params) => async () => {
   }
 };
 
+const readById = (client, params) => async id => {
+  try {
+    const data = await client.get({ Key: { id }, ...params }).promise();
+    return Either.Right(data);
+  } catch (e) {
+    return Either.Left(e);
+  }
+};
+
 export default (client, params) => ({
   read: read(client, params),
+  readById: readById(client, params),
 });
--- src/persistence/dynamoDb/index.test.js
+++ src/persistence/dynamoDb/index.test.js
@@ -45,4 +45,48 @@ describe('Dynamo DB', () => {
       expect(actual).toEqual(Either.Left('Error something went wrong'));
     });
   });
+
+  describe('.readById', () => {
+    const noop = () => {};
+    const params = { table: 'whatever' };
+
+    it('calls client get', () => {
+      const getSpy = jest.fn(() => ({ promise: noop }));
+      const client = { get: getSpy };
+      DynamoDb(client).readById(1001);
+      expect(getSpy).toHaveBeenCalled();
+    });
+
+    it('calls client get with params and id', () => {
+      const getSpy = jest.fn(() => ({ promise: noop }));
+      const client = { get: getSpy };
+
+      DynamoDb(client, params).readById(1001);
+      expect(getSpy).toHaveBeenCalledWith({ table: 'whatever', Key: { id: 1001 } });
+    });
+
+    it('calls promise on get', () => {
+      const promiseSpy = jest.fn();
+      const client = { get: () => ({ promise: promiseSpy }) };
+
+      DynamoDb(client, params).readById();
+      expect(promiseSpy).toHaveBeenCalled();
+    });
+
+    it('returns success with data when promise resolves', async () => {
+      const promiseSpy = () => Promise.resolve([1, 2, 3]);
+      const client = { get: () => ({ promise: promiseSpy }) };
+
+      const actual = await DynamoDb(client, params).readById();
+      expect(actual).toEqual(Either.Right([1, 2, 3]));
+    });
+
+    it('returns error with message when promise rejects', async () => {
+      const promiseSpy = () => Promise.reject('Error something went wrong');
+      const client = { get: () => ({ promise: promiseSpy }) };
+
+      const actual = await DynamoDb(client, params).readById();
+      expect(actual).toEqual(Either.Left('Error something went wrong'));
+    });
+  });
 });
