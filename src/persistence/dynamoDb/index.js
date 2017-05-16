04ae2f966cca90f6a465edf3c4535a8adcf5b04d
Matt Phillips <matt@mattphillips.io>
2017-05-16 10:28:40 +0100

    Refactor duplication

--- src/persistence/dynamoDb/index.js
+++ src/persistence/dynamoDb/index.js
@@ -1,22 +1,17 @@
 import Either from 'data.either';
 
-const read = (client, params) => async () => {
+const executePromise = async promise => {
   try {
-    const data = await client.scan(params).promise();
+    const data = await promise();
     return Either.Right(data);
   } catch (e) {
     return Either.Left(e);
   }
 };
 
-const readById = (client, params) => async id => {
-  try {
-    const data = await client.get({ Key: { id }, ...params }).promise();
-    return Either.Right(data);
-  } catch (e) {
-    return Either.Left(e);
-  }
-};
+const read = (client, params) => () => executePromise(client.scan(params).promise);
+
+const readById = (client, params) => id => executePromise(client.get({ Key: { id }, ...params }).promise);
 
 export default (client, params) => ({
   read: read(client, params),
