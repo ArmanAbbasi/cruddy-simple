6e686c499559bc54bc60392a76fe8119a4fe3542
Matt Phillips <matt@mattphillips.io>
2017-05-17 17:18:17 +0100

    Add initial server

---
+++ src/server/index.js
@@ -0,0 +1,18 @@
+import Koa from 'koa';
+import KoaRouter from 'koa-router';
+import cors from 'koa-cors';
+import bodyParser from 'koa-bodyparser';
+
+import routes from '../routes';
+
+export default (db, host, port, endpoint) => {
+  const app = new Koa();
+  const koaRouter = new KoaRouter({ prefix: endpoint });
+  const router = routes(koaRouter, db);
+
+  app.use(cors());
+  app.use(bodyParser());
+  app.use(router.routes());
+
+  return app.listen(port, () => console.log({ message: `Server running at ${host}:${port}` }));
+};
