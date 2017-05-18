8608408c0d7f7a62695782a61b30fbe831ebc31d
Matt Phillips <matt@mattphillips.io>
2017-05-18 14:13:54 +0100

    Add schema middleware to app

--- src/server/index.js
+++ src/server/index.js
@@ -2,18 +2,22 @@ import Koa from 'koa';
 import KoaRouter from 'koa-router';
 import cors from 'koa-cors';
 import bodyParser from 'koa-bodyparser';
+import Ajv from 'ajv';
 
 import routes from '../routes';
-import { validateContentType } from '../services';
+import { validateBodyWithSchema, validateContentType } from '../services';
 
-export default (db, host, port, endpoint) => {
+export default schema => (db, host, port, endpoint) => {
   const app = new Koa();
   const koaRouter = new KoaRouter({ prefix: `/${endpoint}` });
   const router = routes(koaRouter, db);
 
+  const validator = new Ajv({ allErrors: true }).compile(schema);
+
   app.use(cors());
   app.use(bodyParser());
   app.use(validateContentType('application/json'));
+  app.use(validateBodyWithSchema(validator));
   app.use(router.routes());
 
   return app.listen(port, () => console.log({ message: `Server running at ${host}:${port}` }));
