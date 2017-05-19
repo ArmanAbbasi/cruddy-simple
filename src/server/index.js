90fff1eed208e2f1cdc09a29e41e270368063c77
Matt Phillips <matt@mattphillips.io>
2017-05-19 13:23:46 +0100

    Add basic auth of unsafe methods

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 import Ajv from 'ajv';
+import auth from 'koa-basic-auth';
 import bodyParser from 'koa-bodyparser';
 import cors from 'koa-cors';
 import Koa from 'koa';
@@ -6,15 +7,15 @@ import KoaRouter from 'koa-router';
 import { validate, ui } from 'swagger2-koa';
 
 import routes from '../routes';
-import { validateBodyWithSchema, validateContentType } from '../services';
+import { authUnsafeMethods, validateBodyWithSchema, validateContentType } from '../services';
 
-export default (schema, config, swaggerDoc, logger) => db => {
+export default (schema, config, swaggerDoc, credentials, logger) => db => {
   const { host, port, resource } = config;
 
   const app = new Koa();
 
   const koaRouter = new KoaRouter({ prefix: `/${resource}` });
-  const router = routes(koaRouter, db, logger);
+  const router = routes(koaRouter, db, credentials, logger);
 
   const validator = new Ajv({ allErrors: true }).compile(schema);
 
@@ -22,9 +23,10 @@ export default (schema, config, swaggerDoc, logger) => db => {
   app.use(bodyParser());
   app.use(validateContentType('application/json'));
   app.use(validateBodyWithSchema(validator));
-  app.use(router.routes());
   app.use(validate(swaggerDoc));
   app.use(ui(swaggerDoc, '/swagger'));
+  app.use(authUnsafeMethods(auth(credentials)));
+  app.use(router.routes());
 
   return app.listen(port, () => logger.info(`INFO: Server running at ${host}:${port}`));
 };
