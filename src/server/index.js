d97e29956515a78828791cc3dc5bd67ab7e3bf38
Matt Phillips <matt@mattphillips.io>
2017-05-19 12:09:10 +0100

    Add swagger ui endpoint

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,18 @@
+import Ajv from 'ajv';
+import bodyParser from 'koa-bodyparser';
+import cors from 'koa-cors';
 import Koa from 'koa';
 import KoaRouter from 'koa-router';
-import cors from 'koa-cors';
-import bodyParser from 'koa-bodyparser';
-import Ajv from 'ajv';
+import { validate, ui } from 'swagger2-koa';
 
 import routes from '../routes';
 import { validateBodyWithSchema, validateContentType } from '../services';
 
-export default (schema, config, logger) => db => {
+export default (schema, config, swaggerDoc, logger) => db => {
   const { host, port, resource } = config;
+
   const app = new Koa();
+
   const koaRouter = new KoaRouter({ prefix: `/${resource}` });
   const router = routes(koaRouter, db, logger);
 
@@ -20,6 +23,8 @@ export default (schema, config, logger) => db => {
   app.use(validateContentType('application/json'));
   app.use(validateBodyWithSchema(validator));
   app.use(router.routes());
+  app.use(validate(swaggerDoc));
+  app.use(ui(swaggerDoc, '/swagger'));
 
   return app.listen(port, () => logger.info(`INFO: Server running at ${host}:${port}`));
 };
