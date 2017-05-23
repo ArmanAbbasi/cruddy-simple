e115244ac4276e100e4643718727581793694d1c
Matt Phillips <mattphillips1016@gmail.com>
2017-05-23 16:12:03 +0100

    Add NotFoundError to rexport

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import server from './server';
 import dynamoDb from './persistence/dynamoDb';
+import { NotFoundError } from './utils';
 
-export { server, dynamoDb };
+export { server, dynamoDb, NotFoundError };
